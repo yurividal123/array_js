7e9721a4945bd1cb26399971835929f3ccc12742
Yuri Vidal <yurividal123@hotmail.com>
2023-10-16 18:13:23 -0300

    nova atualização

---
+++ src/async.js
@@ -0,0 +1,38 @@
+function funcaoMuitoPesada() {
+    let execucoes = 0;
+
+    for (let i = 0; i < 1000000000; i++ ) {
+        execucoes++;
+    }
+    return execucoes;
+}
+
+const funcaoMuitoPesadaPromise = new Promise((resolve, reject) => {
+    try {
+        let execucoes = 0;
+
+        for (let i = 0; i < 1000000000; i++ ) {
+            execucoes++;
+        }
+        resolve(execucoes);
+    } catch(e) {
+        reject('Deu erro na interação dos números');
+    }
+})
+
+console.log("inicio")
+// console.log(funcaoMuitoPesada());
+funcaoMuitoPesadaPromise.then(resultado => console.log(resultado)).catch(erro => console.log(erro));
+console.log("fim")
+
+async function execucaoPrincipal() {
+    console.log("inicio")
+
+    // await funcaoMuitoPesadaPromise.then(resultado => console.log(resultado)).catch(erro => console.log(erro));
+    const resultado = await funcaoMuitoPesadaPromise
+    console.log(resultado)
+
+console.log("fim")
+}
+
+execucaoPrincipal()
